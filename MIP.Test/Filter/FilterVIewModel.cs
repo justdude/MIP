dc0147399178cfc35ae7c7e6f229e39d6bcf5569
dude_just_dude <dude.just.dude@gmai.com>
2015-10-12 08:29:19 +0300

    fixes

--- MIP.Test/Filter/FilterVIewModel.cs
+++ MIP.Test/Filter/FilterVIewModel.cs
@@ -27,41 +27,6 @@ namespace MIP.Test.Filter
 		public ListCollectionViewModel<StringItemViewModel> Jobs
 		{ get; set; }
 
-
-		public StringItemViewModel SelectedPerson 
-		{
-			get
-			{
-				return mvSelectedPerson;
-			}
-			set
-			{
-				if (value == mvSelectedPerson)
-					return;
-
-				mvSelectedPerson = value;
-
-				RaisePropertyChanged(() => this.SelectedPerson);
-			}
-		}
-
-		public StringItemViewModel SelectedJob
-		{
-			get
-			{
-				return mvSelectedJob;
-			}
-			set
-			{
-				if (value == mvSelectedJob)
-					return;
-
-				mvSelectedJob = value;
-
-				RaisePropertyChanged(() => this.SelectedJob);
-			}
-		}
-
 		protected override void OnTokenChanged()
 		{
 			base.OnTokenChanged();
@@ -69,8 +34,8 @@ namespace MIP.Test.Filter
 			Jobs.Items.Add(new StringItemViewModel("Work1"));
 			Jobs.Items.Add(new StringItemViewModel("Work2"));
 
-			Jobs.Items.Add(new StringItemViewModel("Ivan"));
-			Jobs.Items.Add(new StringItemViewModel("Andrew"));
+			Persons.Items.Add(new StringItemViewModel("Ivan"));
+			Persons.Items.Add(new StringItemViewModel("Andrew"));
 		}
 
 		public override void Cleanup()
